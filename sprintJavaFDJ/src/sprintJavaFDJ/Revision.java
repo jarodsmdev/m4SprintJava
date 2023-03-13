66160c12511e7777958b23a46242883a4e35b3bc
Jarod Smith <lbriones.dev@gmail.com>
2023-03-13 13:43:52 -0300

    add Revision.java v1

--- sprintJavaFDJ/src/sprintJavaFDJ/Revision.java
+++ sprintJavaFDJ/src/sprintJavaFDJ/Revision.java
@@ -1,5 +1,19 @@
 package sprintJavaFDJ;
 
 public class Revision {
+	private int idRevision;
+	private int idVisita;
+	private String nombreRevision;
+	private int estado;
+	
+	public Revision() {}
+	
+	public Revision(int idRevision, int idVisita, String nombreRevision, int estado) {
+		this.idRevision = idRevision;
+		this.idVisita = idVisita;
+		this.nombreRevision = nombreRevision;
+		this.estado = estado;
+	}
+	
 
 }
