7680cc9c1307c8e44542878d6ee0c83a77ca8031
Jarod Smith <lbriones.dev@gmail.com>
2023-03-20 10:14:30 -0300

    Añade Clases de Test y JavaDoc

--- sprintJavaFDJ/src/testSprintJavaFDJ/TestUsuario.java
+++ sprintJavaFDJ/src/testSprintJavaFDJ/TestUsuario.java
@@ -1,26 +1,14 @@
 package testSprintJavaFDJ;
 
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import sprintJavaFDJ.Usuario;
-
 public class TestUsuario {
 
-	private final Usuario usuario = new Usuario();
-	String rptaCorrecta;
-	String rptaIncorrecta;
-	
-	@Test 
-	public void testObtenerFechaNac() {
-		int dia = 20;
-		int mes = 7;
-		int anio = 1997;
-		
-		rptaCorrecta = usuario.obtenerFechaNac();
-		assertEquals(dia + "/" + mes + "/" + anio, rptaCorrecta);
-		
+	@Test
+	public void test() {
+		fail("Not yet implemented");
 	}
+
 }
